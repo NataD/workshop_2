0dbee65f3b21e3c657f065a0c75020034216b7ad
Nataliia Dobrowolska <nmdobrowolska@gmail.com>
2018-03-03 12:00:30 +0100

    Menu

--- 4_Menu/js/app.js
+++ 4_Menu/js/app.js
@@ -1,3 +1,40 @@
 /**
  * Created by Jacek on 2015-12-16.
  */
+document.addEventListener('DOMContentLoaded', function () {
+
+   // var navigationDiv = document.querySelector('.nav');
+    var list = document.querySelector('.nav ul');
+    console.log(list);
+    var listEl = list.children;
+    console.log(listEl);
+
+
+    [...listEl].forEach(function(element){
+    element.addEventListener('mouseover', function (event) {
+        //console.log('Hello');
+        var innerUl = element.querySelector('ul');
+        if (innerUl !== null){
+            //console.log(innerUl);
+            innerUl.style.display = 'inline-block';
+        }
+
+    });
+
+
+    });
+
+
+   [...listEl].forEach(function(element){
+    element.addEventListener('mouseout', function (event) {
+        console.log('Bye');
+
+        var innerUl = element.querySelector('ul');
+        if (innerUl !== null){
+            //console.log(innerUl);
+            innerUl.style.display = 'none';
+        }
+    });
+    });
+
+});
