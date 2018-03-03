64c06a85a87eff66b1f3a0bad5a631f8a2e0b05e
Nataliia Dobrowolska <nmdobrowolska@gmail.com>
2018-03-03 11:21:04 +0100

    Gallery with filter

--- 3_GalleryWithFilter/js/app.js
+++ 3_GalleryWithFilter/js/app.js
@@ -14,18 +14,78 @@ document.addEventListener('DOMContentLoaded', function(){
     var hideBtn = document.querySelector('#hideButton');
     console.log(hideBtn);
 
-    var inputText = document.querySelector('#tagInput');
-    console.log(inputText);
+    var inputField = document.querySelector('#tagInput');
+    console.log(inputField);
+
 
 
     showBtn.addEventListener('click', function (event) {
 
+        //write inout value to variable
+        var inputText = inputField.value;
+        console.log(inputText);
 
-        console.log('Hello');
+        //clean input
+        inputField.value = '';
+
+
+
+
+
+        [...images].forEach(function (element) {
+            //var imagesTags = [];
+            //imagesTags.push(element.dataset.tag);
+           //console.log(imagesTags);
+            var imagesTags = element.dataset.tag;
+            //console.log(imagesTags.indexOf(inputText));
+
+            if(imagesTags.indexOf(inputText) !== -1){
+                //console.log(imagesTags);
+                //images.hasAttribute('data-tag', inputText)
+                element.classList.remove('invisible');
+            } else {
+                element.classList.add('invisible');
+            }
+
+
+            //else {
+           //     console.log('');
+           // }
+        });
+
+
+
     });
 
 
     hideBtn.addEventListener('click', function (event) {
-        console.log('Hello');
+       // console.log('Hello');
+
+        //write inout value to variable
+        var inputText = inputField.value;
+        console.log(inputText);
+
+        //clean input
+        inputField.value = '';
+
+        [...images].forEach(function (element) {
+            //var imagesTags = [];
+            //imagesTags.push(element.dataset.tag);
+            //console.log(imagesTags);
+            var imagesTags = element.dataset.tag;
+            //console.log(imagesTags.indexOf(inputText));
+
+            if(imagesTags.indexOf(inputText) !== -1){
+                //console.log(imagesTags);
+                //images.hasAttribute('data-tag', inputText)
+                element.classList.add('invisible');
+            }
+
+
+            //else {
+            //     console.log('');
+            // }
+        });
+
     });
 });
